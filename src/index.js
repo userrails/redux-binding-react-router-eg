260125c4a08fe747b3860d224d7f383f2cd213f6
Shiv Raj Badu <shivrajbadu@gmail.com>
2019-03-25 19:09:32 +0545

    react-hot-loader added to tweak components in realtime

--- src/index.js
+++ src/index.js
@@ -4,19 +4,33 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import configureStore, { history } from './configureStore'
+import { AppContainer } from 'react-hot-loader'
 
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore()
 
+const render = () => {
 ReactDOM.render(
-  <Provider store={store}>
-    <App  history={history}  />
-  </Provider>,
-
+  <AppContainer>
+    <Provider store={store}>
+      <App  history={history}  />
+    </Provider>
+  </AppContainer>,
   document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+render()
+
+// Hot reloading
+if (module.hot) {
+  // Reload components
+  module.hot.accept('./App', () => {
+    render()
+  })
+}
